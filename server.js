b02407f56d18a7f1dee2a037b44f3fbd28c58273
axetroy <troy450409405@gmail.com>
2017-07-16 16:11:37 +0800

    update

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const github = require('./lib/github');
+
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
@@ -16,7 +18,15 @@ app.get('/', function(req, res) {
 });
 
 app.get('/:owner/:repo.svg', function(req, res) {
-  res.send('you gonna get svg')
+  const { owner, repo } = req.param.params;
+  github
+    .get(`/repos/${owner}/${repo}`)
+    .then(function(response) {
+      res.status(200).send(`hello ${owner}, repo: ${repo}`);
+    })
+    .catch(function() {
+      res.status(200).send(`hello ${owner}, repo: ${repo} fail`);
+    });
 });
 
 app.listen(PORT, function() {
